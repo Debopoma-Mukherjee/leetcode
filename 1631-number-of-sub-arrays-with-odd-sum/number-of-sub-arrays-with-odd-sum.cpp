757cdf167cb4ac360ab08365a69e86a47bf882d6
Debopoma-Mukherjee <100709038+Debopoma-Mukherjee@users.noreply.github.com>
2025-02-25 21:50:07 +0530

    Time: 24 ms (10.60%) | Memory: 121.6 MB (20.67%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -28,12 +28,12 @@ public:
         sum += arr[i];
         if (sum % 2 != 0)
         {
-            cnt = (cnt + dp[0]) % (1000000007); 
+            cnt = (cnt + dp[0]) % (1000000007); //oddsum=newoddsum+prevevensum;
             dp[1]++; 
         } else
         {
             cnt = (cnt + dp[1]) % (1000000007); 
-            dp[0]++; 
+            dp[0]++; //oddsum=newevensum+allprevoddsum
         }
         oddSumSubArrays(arr, i + 1, n, sum, cnt, dp);
     }
@@ -42,7 +42,7 @@ public:
         int n = arr.size();
         int cnt = 0;
         unordered_map<int, int> dp;
-        dp[0] = 1; 
+        dp[0] = 1; //initial sum 0 (even)
         oddSumSubArrays(arr, 0, n, 0, cnt, dp);
         return cnt;
     }
