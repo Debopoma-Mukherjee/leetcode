e9d3d725f34dd37a6f269177e4bdccde9496cde1
Debopoma-Mukherjee <100709038+Debopoma-Mukherjee@users.noreply.github.com>
2025-02-28 22:02:53 +0530

    Time: 43 ms (76.38%) | Memory: 104.7 MB (38.04%) - LeetSync

---
+++ 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long minimumRemoval(vector<int>& beans) {
+        sort(beans.begin(),beans.end());
+        long long n=beans.size();
+        long long total=0;
+        long long minm=99999999999999;
+        for(int i=0;i<n;i++)
+        {
+            total=total+beans[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            long long remaining = total - (long long)(beans[i]) * (long long)(n - i);
+        minm = min(minm, remaining);
+        }
+        return minm;
+    }
+};
