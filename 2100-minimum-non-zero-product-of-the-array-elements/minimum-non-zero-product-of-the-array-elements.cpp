e313fcca17e0972f48e91435c21abfd156b49ea7
Debopoma-Mukherjee <100709038+Debopoma-Mukherjee@users.noreply.github.com>
2025-05-28 17:35:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (7.79%) - LeetSync

--- 2100-minimum-non-zero-product-of-the-array-elements/minimum-non-zero-product-of-the-array-elements.cpp
+++ 2100-minimum-non-zero-product-of-the-array-elements/minimum-non-zero-product-of-the-array-elements.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    /*long long binary_exponentiation(long long x,int n)
+    long long binary_exponentiation(long long x,long long  n)
     {
         long long ans=1;
         int mod=1e9+7;
+        x %= mod;
         while(n>0)
         {
             if(n%2==0)
@@ -22,32 +23,11 @@ public:
     int minNonZeroProduct(int p) 
     {
         int mod=1e9+7;
-        long long last_value=(binary_exponentiation(2,p))-1;
+        long long last_value=(long long) pow(2,p)- 1;
         long long product=(binary_exponentiation(last_value-1,(last_value-1)/2));
         long long ans=((product)*(last_value%mod))%mod;
         return ans;
-    }*/
+    }
  
-    const long long mod = 1e9 + 7;
-
-    long long binary_exponentiation(long long x, long long n) {
-        long long ans = 1;
-        x %= mod;
-        while (n > 0) {
-            if (n % 2 == 1)
-                ans = (ans * x) % mod;
-            x = (x * x) % mod;
-            n /= 2;
-        }
-        return ans;
-    }
-
-    int minNonZeroProduct(int p) {
-        long long last_value =(long long) pow(2,p)- 1;
-        long long product = binary_exponentiation(last_value - 1, last_value / 2);
-        long long ans = (product * (last_value % mod)) % mod;
-        return ans;
-    }
-
-
+    
 };
